ea9503e5a30581c0bd640364df233f50b62c8882
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-07-07 12:30:12 +0630

    resolving templateInputException before java config

--- sec02-getting-start-java/src/main/java/com/jdc/spring/javaconfig/WebMvcConfiguration.java
+++ sec02-getting-start-java/src/main/java/com/jdc/spring/javaconfig/WebMvcConfiguration.java
@@ -1,20 +1,45 @@
 package com.jdc.spring.javaconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.jdc.spring.javaconfig.controller")
+@ComponentScan(basePackages = "com.jdc.spring.javaconfig")
 public class WebMvcConfiguration implements WebMvcConfigurer{
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
-		registry.jsp("/views/", ".jsp");
+	
+	@Bean
+	SpringResourceTemplateResolver templateResolver() {
+		var resolver = new SpringResourceTemplateResolver();
+		resolver.setPrefix("/views/");
+		resolver.setSuffix(".html");
+		resolver.setCacheable(false);
+		
+		return resolver;
+	}
+	
+	@Bean
+	SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+		var engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver);
+		
+		return engine;
+	}
+	
+	@Bean
+	ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+		var viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine);
+		
+		return viewResolver;
 	}
 	
 	@Override
