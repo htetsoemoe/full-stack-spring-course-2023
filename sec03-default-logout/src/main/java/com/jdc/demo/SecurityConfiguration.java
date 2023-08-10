5908fa04726f7d0841832f6ba10c36f28591ecd6
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-08-10 11:06:56 +0630

    default logout in spring boot security

---
+++ sec03-default-logout/src/main/java/com/jdc/demo/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.jdc.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfiguration implements WebMvcConfigurer{
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/member").setViewName("member");
+		registry.addRedirectViewController("/", "/home");
+	}
+	
+	// In Spring Boot Security, there is a auto UserDetailsService Bean
+	// SecurityFilterChain
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+		
+		security.authorizeHttpRequests(request -> {
+			request.requestMatchers("/", "/home").permitAll();
+			request.anyRequest().fullyAuthenticated();
+		});
+		
+		// using default login
+		security.formLogin(form -> {});
+		
+		return security.build();
+	}
+}
